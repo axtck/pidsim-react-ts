a9e3efb87cc66b02c98d2fa91bb48e3b85e2b30c
Alexander Tack <alexander.tack@student.vives.be>
2021-04-18 22:25:11 +0200

    Add chart

---
+++ src/data/dummy.js
@@ -0,0 +1,272 @@
+export const countries = [
+    {
+        id: "japan",
+        color: "hsl(280, 70%, 50%)",
+        data: [
+            {
+                x: "plane",
+                y: 59
+            },
+            {
+                x: "helicopter",
+                y: 163
+            },
+            {
+                x: "boat",
+                y: 71
+            },
+            {
+                x: "train",
+                y: 298
+            },
+            {
+                x: "subway",
+                y: 105
+            },
+            {
+                x: "bus",
+                y: 274
+            },
+            {
+                x: "car",
+                y: 132
+            },
+            {
+                x: "moto",
+                y: 254
+            },
+            {
+                x: "bicycle",
+                y: 267
+            },
+            {
+                x: "horse",
+                y: 198
+            },
+            {
+                x: "skateboard",
+                y: 211
+            },
+            {
+                x: "others",
+                y: 144
+            }
+        ]
+    },
+    {
+        id: "france",
+        color: "hsl(203, 70%, 50%)",
+        data: [
+            {
+                x: "plane",
+                y: 216
+            },
+            {
+                x: "helicopter",
+                y: 15
+            },
+            {
+                x: "boat",
+                y: 117
+            },
+            {
+                x: "train",
+                y: 20
+            },
+            {
+                x: "subway",
+                y: 14
+            },
+            {
+                x: "bus",
+                y: 200
+            },
+            {
+                x: "car",
+                y: 54
+            },
+            {
+                x: "moto",
+                y: 195
+            },
+            {
+                x: "bicycle",
+                y: 173
+            },
+            {
+                x: "horse",
+                y: 4
+            },
+            {
+                x: "skateboard",
+                y: 7
+            },
+            {
+                x: "others",
+                y: 191
+            }
+        ]
+    },
+    {
+        id: "us",
+        color: "hsl(315, 70%, 50%)",
+        data: [
+            {
+                x: "plane",
+                y: 66
+            },
+            {
+                x: "helicopter",
+                y: 129
+            },
+            {
+                x: "boat",
+                y: 143
+            },
+            {
+                x: "train",
+                y: 166
+            },
+            {
+                x: "subway",
+                y: 204
+            },
+            {
+                x: "bus",
+                y: 206
+            },
+            {
+                x: "car",
+                y: 216
+            },
+            {
+                x: "moto",
+                y: 45
+            },
+            {
+                x: "bicycle",
+                y: 26
+            },
+            {
+                x: "horse",
+                y: 4
+            },
+            {
+                x: "skateboard",
+                y: 90
+            },
+            {
+                x: "others",
+                y: 268
+            }
+        ]
+    },
+    {
+        id: "germany",
+        color: "hsl(49, 70%, 50%)",
+        data: [
+            {
+                x: "plane",
+                y: 62
+            },
+            {
+                x: "helicopter",
+                y: 177
+            },
+            {
+                x: "boat",
+                y: 177
+            },
+            {
+                x: "train",
+                y: 153
+            },
+            {
+                x: "subway",
+                y: 208
+            },
+            {
+                x: "bus",
+                y: 91
+            },
+            {
+                x: "car",
+                y: 80
+            },
+            {
+                x: "moto",
+                y: 269
+            },
+            {
+                x: "bicycle",
+                y: 42
+            },
+            {
+                x: "horse",
+                y: 267
+            },
+            {
+                x: "skateboard",
+                y: 190
+            },
+            {
+                x: "others",
+                y: 12
+            }
+        ]
+    },
+    {
+        id: "norway",
+        color: "hsl(266, 70%, 50%)",
+        data: [
+            {
+                x: "plane",
+                y: 147
+            },
+            {
+                x: "helicopter",
+                y: 220
+            },
+            {
+                x: "boat",
+                y: 62
+            },
+            {
+                x: "train",
+                y: 65
+            },
+            {
+                x: "subway",
+                y: 277
+            },
+            {
+                x: "bus",
+                y: 54
+            },
+            {
+                x: "car",
+                y: 61
+            },
+            {
+                x: "moto",
+                y: 114
+            },
+            {
+                x: "bicycle",
+                y: 201
+            },
+            {
+                x: "horse",
+                y: 11
+            },
+            {
+                x: "skateboard",
+                y: 289
+            },
+            {
+                x: "others",
+                y: 153
+            }
+        ]
+    }
+];
--- src/pages/General/Simulator/Simulator.tsx
+++ src/pages/General/Simulator/Simulator.tsx
@@ -1,11 +1,15 @@
 import React, { FunctionComponent } from 'react';
+import LineChart from '../../../components/Chart/LineChart';
+import { countries } from "../../../data/dummy";
 
 interface SimulatorProps { };
 
 const Simulator: FunctionComponent<SimulatorProps> = () => {
     return (
         <div>
-
+            <div style={{ height: 400 }}>
+                <LineChart data={countries} />
+            </div>
         </div>
     );
 }
